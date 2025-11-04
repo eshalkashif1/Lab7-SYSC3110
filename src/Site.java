a202af973baa44b55783a4d47dcb685b064b3ba0
eshalkashif1 <eshalkashif05@gmail.com>
2025-11-03 20:34:15 -0500

    Initial version before refactoring

---
+++ src/Site.java
@@ -0,0 +1,10 @@
+public class Site {
+    protected double _units;
+    protected double _rate;
+    public static final double TAX_RATE = 0.13;
+
+    public Site(double _units, double _rate){
+        this._units = _units;
+        this._rate = _rate;
+    }
+}
